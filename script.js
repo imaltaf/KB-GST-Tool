82c172bdf4b735533f3147f821cfa194a32c9476
Altaf <imaltaf@proton.me>
2023-10-28 14:26:58 +0530

    chenged pincode only numbers and added states in code

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
+const copyButton = document.getElementById("copyButton");
+const resetButton = document.getElementById("resetButton");
+const formattedContent = document.getElementById("formattedContent");
 
-  const copyButton = document.getElementById("copyButton");
-  const resetButton = document.getElementById("resetButton");
-  const formattedContent = document.getElementById("formattedContent");
+copyButton.addEventListener("click", () => {
+  const tradeName = document.getElementById("tradeName").value;
+  const natureOfBusiness = document.getElementById("natureOfBusiness").value;
+  const line1 = document.getElementById("line1").value;
+  const line2 = document.getElementById("line2").value;
+  const pinCode = document.getElementById("pinCode").value;
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value; // Get the state from the input field
 
-  // Function to remove special characters
-  function removeSpecialCharacters(inputString) {
-    return inputString.replace(/[^\w\s]/gi, ""); // This regex removes special characters
+  // Check if the state is empty
+  if (state.trim() === "") {
+    alert("Please enter the state.");
+    return;
   }
 
-  copyButton.addEventListener("click", () => {
-    const tradeName = removeSpecialCharacters(document.getElementById("tradeName").value);
-    const natureOfBusiness = removeSpecialCharacters(document.getElementById("natureOfBusiness").value);
-    const line1 = removeSpecialCharacters(document.getElementById("line1").value);
-    const line2 = removeSpecialCharacters(document.getElementById("line2").value);
-    const pinCode = removeSpecialCharacters(document.getElementById("pinCode").value);
-    const city = removeSpecialCharacters(document.getElementById("city").value);
-    const state = removeSpecialCharacters(document.getElementById("state").value);
+  const formattedText =
+    `Trade Name/Name of Business: ${tradeName} | ` +
+    `Nature of Business/Line of Business/Type of Business: ${natureOfBusiness} | ` +
+    `Line1: ${line1} | ` +
+    `Line2: ${line2} | ` +
+    `PinCode: ${pinCode} | ` +
+    `City: ${city} | ` +
+    `State: ${state}`;
 
-    // Check if any of the required fields are empty
-    if (!tradeName || !natureOfBusiness || !line1 || !line2 || !pinCode || !city || !state) {
-      alert("Please fill in all the required fields.");
-      return; // Exit the function if fields are empty
-    }
+  formattedContent.value = formattedText;
+  formattedContent.select();
+  document.execCommand("copy");
+});
 
-    const formattedText =
-      `Trade Name/Name of Business: ${tradeName} | ` +
-      `Nature of Business/Line of Business/Type of Business: ${natureOfBusiness} | ` +
-      `Line1: ${line1}| ` +
-      `Line2: ${line2}| ` +
-      `PinCode: ${pinCode}| ` +
-      `City: ${city}| ` +
-      `State: ${state}`;
-
-    formattedContent.value = formattedText;
-    formattedContent.select();
-    document.execCommand("copy");
+resetButton.addEventListener("click", () => {
+  document.querySelectorAll("input[type='text']").forEach((input) => {
+    input.value = "";
   });
-
-  resetButton.addEventListener("click", () => {
-    document.querySelectorAll("input[type='text']").forEach(input => {
-      input.value = "";
-    });
-    formattedContent.value = "";
-  });
-
+  document.getElementById("pinCode").value = ""; // Clear the pinCode input
+  document.getElementById("state").value = ""; // Clear the state input
+  formattedContent.value = "";
+});
