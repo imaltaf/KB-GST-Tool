1bb32420c2c423215b05c3aaa23c23b213de5c29
Altaf <imaltaf@proton.me>
2023-10-28 22:39:31 +0530

    recode in JS file

--- script.js
+++ script.js
@@ -1,19 +1,96 @@
-const copyButton = document.getElementById("copyButton");
-const resetButton = document.getElementById("resetButton");
-const formattedContent = document.getElementById("formattedContent");
+// List of all states in India
+const indianStates = [
+  "Andaman and Nicobar Islands",
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chandigarh",
+  "Chhattisgarh",
+  "Dadra and Nagar Haveli and Daman and Diu",
+  "Delhi",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Ladakh",
+  "Lakshadweep",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Puducherry",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal"
+];
 
-copyButton.addEventListener("click", () => {
-  const tradeName = document.getElementById("tradeName").value;
-  const natureOfBusiness = document.getElementById("natureOfBusiness").value;
-  const line1 = document.getElementById("line1").value;
-  const line2 = document.getElementById("line2").value;
-  const pinCode = document.getElementById("pinCode").value;
-  const city = document.getElementById("city").value;
-  const state = document.getElementById("state").value; // Get the state from the input field
+// Function to remove special characters
+function removeSpecialCharacters(inputString) {
+  return inputString.replace(/[^\w\s]/gi, "");
+}
 
-  // Check if the state is empty
-  if (state.trim() === "") {
-    alert("Please enter the state.");
+// Function to restrict "PinCode" input to only allow numbers
+function restrictPincodeInput() {
+  const pincodeInput = document.getElementById("pinCode");
+  pincodeInput.addEventListener("input", () => {
+    // Remove non-numeric characters
+    pincodeInput.value = pincodeInput.value.replace(/\D/g, "");
+  });
+}
+
+// Function to provide auto-suggestion for "State" input
+function setupStateAutosuggestion() {
+  const stateInput = document.getElementById("state");
+  stateInput.addEventListener("input", () => {
+    const inputText = stateInput.value.toLowerCase();
+    const suggestions = indianStates.filter(state => state.toLowerCase().includes(inputText));
+
+    // Display the suggestions
+    const suggestionList = document.getElementById("stateSuggestions");
+    suggestionList.innerHTML = "";
+    suggestions.forEach(suggestion => {
+      const listItem = document.createElement("li");
+      listItem.textContent = suggestion;
+      listItem.addEventListener("click", () => {
+        stateInput.value = suggestion;
+        suggestionList.innerHTML = "";
+      });
+      suggestionList.appendChild(listItem);
+    });
+  });
+
+  // Clear the suggestions when the input loses focus
+  stateInput.addEventListener("blur", () => {
+    document.getElementById("stateSuggestions").innerHTML = "";
+  });
+}
+
+// Function to copy the formatted content
+function copyFormattedContent() {
+  const tradeName = removeSpecialCharacters(document.getElementById("tradeName").value);
+  const natureOfBusiness = removeSpecialCharacters(document.getElementById("natureOfBusiness").value);
+  const line1 = removeSpecialCharacters(document.getElementById("line1").value);
+  const line2 = removeSpecialCharacters(document.getElementById("line2").value);
+  const pinCode = removeSpecialCharacters(document.getElementById("pinCode").value);
+  const city = removeSpecialCharacters(document.getElementById("city").value);
+  const state = removeSpecialCharacters(document.getElementById("state").value);
+
+  // Check if any of the required fields are empty
+  if (!tradeName || !natureOfBusiness || !line1 || !line2 || !pinCode || !city || !state) {
+    alert("Please fill in all the required fields.");
     return;
   }
 
@@ -26,16 +103,26 @@ copyButton.addEventListener("click", () => {
     `City: ${city} | ` +
     `State: ${state}`;
 
+  const formattedContent = document.getElementById("formattedContent");
   formattedContent.value = formattedText;
   formattedContent.select();
   document.execCommand("copy");
-});
+}
 
-resetButton.addEventListener("click", () => {
-  document.querySelectorAll("input[type='text']").forEach((input) => {
+// Function to reset input fields
+function resetInputFields() {
+  document.querySelectorAll("input[type='text']").forEach(input => {
     input.value = "";
   });
-  document.getElementById("pinCode").value = ""; // Clear the pinCode input
-  document.getElementById("state").value = ""; // Clear the state input
-  formattedContent.value = "";
+  document.getElementById("formattedContent").value = "";
+}
+
+// Run the functions when the page loads
+window.addEventListener("load", () => {
+  restrictPincodeInput();
+  setupStateAutosuggestion();
+
+  // Attach click event handlers to the "Copy" and "Reset" buttons
+  document.getElementById("copyButton").addEventListener("click", copyFormattedContent);
+  document.getElementById("resetButton").addEventListener("click", resetInputFields);
 });
